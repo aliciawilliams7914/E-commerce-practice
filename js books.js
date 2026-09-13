4daaa66e90c528a4d1a2387d6504014ef9905772
https://github.com/aliciawilliams7914/exclusive-eportfolio.git <mykitchendesignsolution@gmail.com>
2026-09-12 20:40:49 -0400

    changes

--- js books.js
+++ js books.js
@@ -1,6 +1,7 @@
 function renderBooks() {
     const booksWrapper = document.querySelector('.books');
  booksWrapper.innerHTML = 
+ `
 <div class="book">
 <figure class="book__img--wrapper">
 <img class="book__img" src="cracking-the-coding-interview.png" alt="">
@@ -15,11 +16,10 @@ function renderBooks() {
 <i class="fas fa-star"></i>
 <i class="fas fa-star-half-alt"></i>
 </div>
-<div class="book__price">
-<span class="book__price--normal">$59.99</span> $14.95
+<div class="book__price">0
+0<span class="book__price--normal">$59.99</span> $14.95
 </div>
-</div> 
-    }
+0</div> `  }
 setTimeout(() => {
 renderBooks();
 });
